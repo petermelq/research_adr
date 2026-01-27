8c4ddf177f0cc1032980b565faacf5cd3fa5ab10
Peter Malonis <peterm@elequincapital.com>
2026-01-27 17:13:40 -0500

    adding

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import pandas_market_calendars as mcal
 import numpy as np
 from glob import glob
+from linux_xbbg import blp
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def get_partition_dates(partition_path):
@@ -42,3 +43,39 @@ def load_params():
         params = yaml.safe_load(f)
 
     return params
+
+def get_etf_holdings(etf_name, asof, ca_adj='none'):
+    ##
+    ## Get ETF Holdings
+    ##
+    bquery = blp.BlpQuery(timeout=10000).start()        
+    bqlx = (
+        f"for(holdings('{etf_name}', dates={asof})) "
+        "get(NAME().VALUE, ID().SOURCE, ID().POSITION, ID_SEDOL1().VALUE) "
+        f"with(ca_adj={ca_adj})"
+    )
+    df = bquery.bql(expression=bqlx)
+    df = df[['security', 'field', 'value']]
+    df = df.loc[(df['security'].str.contains(' Curncy')==False),:]
+
+    ## check return format of dataframe to determine how to process:
+    if 'field' in df.columns :
+        df = df.pivot(index='security', columns='field', values='value').reset_index()
+
+    #df.columns.name = None
+    df = df.rename(columns={
+        'id' : 'bbg_ticker',
+        'ID().POSITION': 'shares',
+        'NAME().VALUE': 'name',
+        'TICKER().VALUE': 'ticker',
+        'ID_SEDOL1().VALUE' : 'sedol',
+        'ID().SOURCE': 'source',
+        'security': 'id',
+    })
+
+    df['as_of_date'] = asof
+    df['shares'] = pd.to_numeric(df['shares'])
+
+    bquery = blp.BlpQuery().stop()
+
+    return df
